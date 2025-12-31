75b729bc15fe62d769b09be8ba05747e6d625206
pavanraj8943 <pavan.64j@gmail.com>
2025-12-31 12:58:43 +0530

    service section added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets'
 import Title from './Title'
-
+import ServiceCard from './ServiceCard'
 
 const Services = () => {
 
@@ -34,7 +34,11 @@ const Services = () => {
 
         <Title title="How can we help?" desc="From strategy to execution, we craft digital solutions that move your business forward."/>
 
-     
+        <div className='flex flex-col md:grid grid-cols-2'>
+            {servicesData.map((service, index)=>(
+                <ServiceCard key={index} service={service} index={index}/>
+            ))}
+        </div>
     </div>
   )
 }
