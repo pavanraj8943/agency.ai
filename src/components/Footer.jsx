1d4caff038338fa8280a3ea00f8c12c609ade178
pavanraj8943 <pavan.64j@gmail.com>
2025-12-31 15:36:00 +0530

    Contuct and Footer pages completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer=({theme,setTheme})=>{
     return(
         
 <footer class="bg-neutral-primary-soft mt-6">
-    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 dark:text-white">
+    <div class="mx-auto w-full max-w-screen-x p-4 py-6 lg:py-8 dark:text-white">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
