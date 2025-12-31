2ebc5ff32100f3f216ee27f311a3d5f44672c064
pavanraj8943 <pavan.64j@gmail.com>
2025-12-31 13:05:21 +0530

    services section completed

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -13,8 +13,11 @@ const ServiceCard = ({service, index}) => {
     }
 
   return (
-    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shodow-2xl shadow-gray-100 dark:shadow-white/10' onMouseEnter={()=>setVisible(true)} onMouseLeave={()=>setVisible(false)} ref={divRef} onMouseMove={handleMouseMove}>
-        <div className={`pointer-events-none blur-2xl rounded-full  from-blue-500 via-indigo-500 to-purple-500  absolute z-0 transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70' : 'opacity-0'}`} style={{top: position.y - 150, left: position.x - 150}} />
+    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shodow-2xl shadow-gray-100 dark:shadow-white/10'
+     onMouseEnter={()=>setVisible(true)} onMouseLeave={()=>setVisible(false)} ref={divRef} onMouseMove={handleMouseMove}>
+        <div className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500
+             w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten 
+             ${visible ? 'opacity-70' : 'opacity-0'}`} style={{top: position.y - 150, left: position.x - 150}} />
 
             <div className='flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative'>
                 <div className='bg-gray-100 dark:bg-gray-700 rounded-full'>
